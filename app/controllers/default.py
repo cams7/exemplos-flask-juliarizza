04d3dabdb3b64ee99da069fbfb06f0525e0c10a6
César A. Magalhães <ceanma@gmail.com>
2017-05-19 18:35:55 -0300

    Da playlist (Curso de Flask ) do Youtube, foi realizada a (Aula 8). Acesse essa aula através do endereço (https://www.youtube.com/watch?v=Q_SpLfhFiK8&index=9&list=PL3BqW_m3m6a05ALSBW02qDXmfDKIip2KX)

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,20 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from flask import render_template
 from app import app
 
-@app.route('/hello')
 @app.route('/')
 def index():
-    return 'Hello world!'
-
-@app.route('/greeting', defaults={'name':None})
-@app.route('/greeting/<name>')
-def greeting(name):
-    if name:
-        return 'Olá, %s!' % name
-
-    return 'Olá, usuário!'
-
-@app.route('/user/<int:id>', methods=['GET'])
-def user(id):
-    return 'User id is %i!' % id
+    return render_template('index.html')
